6d39a5d27f167cd2bbbba65779379b3a4dea0650
XxxxPrInCeSs_Of_TrAcToRsxxxX <camille.aubret@viacesi.fr>
2018-12-06 16:39:44 +0100

    broken

---
+++ Masterchief Project/UnitTestProject1/Model/DBConnectTests.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Kitchen.Model;
+
+
+
+namespace KitchenTests
+{
+    [TestClass]
+    public class DBConnectTests
+    {
+        [TestMethod]
+        public void ConstructTest()
+        {
+          db = new Kitchen.Model.DBConnect;
+            Assert.AreEqual(0, 0);
+        }
+    }
+}
