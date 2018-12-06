eb0fd34a90d3d893e6e0dbe29bcb4467bfd24193
Matthis-M <matthis.moyon@hotmail.com>
2018-12-06 21:54:51 +0100

    Sockets tests updates
    Can't understand how I can't make it works but well

---
+++ Masterchief Project/Kitchen/Model/Receiver.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Kitchen.Model
+{
+    class Receiver
+    {
+
+
+
+        // Start the listening thread
+        private void StartListening()
+        {
+            Thread listeningThread = new Thread(ListeningThread);
+
+
+        }
+
+
+        // Thread listening to the socket to get messages.
+        private void ListeningThread()
+        {
+
+        }
+
+        // Initialize the connexion with the 
+    }
+}
+
---
+++ Masterchief Project/KitchenTests/Model/ExchangeDeskTests.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace KitchenTests.Model
+{
+    [TestClass]
+    public class ExchangeDeskTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
