6f94b0f903227da12be969e370a07cbd6e7f771b
XxxxPrInCeSs_Of_TrAcToRsxxxX <camille.aubret@viacesi.fr>
2018-12-11 10:51:24 +0100

    DBtest add

---
+++ Masterchief Project/KitchenTests/Model/DBConnectTests.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Kitchen;
+
+namespace KitchenTests
+{
+    [TestClass]
+    public class DBConnectTests
+    {
+        DBConnect Connectiontest = null;
+
+        [TestMethod]
+        public void TestDBConnectdatasrc()
+        {
+            Connectiontest = new DBConnect("MasterChiefDB");
+            Assert.AreEqual("HENDHOLD", Connectiontest.Datasrc);
+        }
+
+        [TestMethod]
+        public void TestDBConnect_Try()
+        {
+            try
+            {
+                Connectiontest = new DBConnect("MasterChiefDB");
+                Assert.IsTrue(true);
+            }
+            catch(Exception)
+            {
+                Assert.Fail();
+            }
+        }
+
+        [TestMethod]
+        public void TestDBConnect_Catch()
+        {
+            try
+            {
+                Connectiontest = new DBConnect("LOL");
+                Assert.Fail();
+            }
+            catch (Exception)
+            {
+                Assert.IsTrue(true);
+            }
+        }
+    }
+}
