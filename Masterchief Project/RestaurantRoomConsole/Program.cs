7ecb0a24a32880cdab70cb52b6422acb8c04b51b
loick legay <loick.legay@orange.fr>
2018-12-11 09:06:18 +0100

    Update console restaurant

--- Masterchief Project/RestaurantRoomConsole/Program.cs
+++ Masterchief Project/RestaurantRoomConsole/Program.cs
@@ -4,12 +4,14 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+
 namespace RestaurantRoomConsole
 {
     class Program
     {
         static void Main(string[] args)
         {
+            Conntroller controler = new Conntroller();
         }
     }
 }
