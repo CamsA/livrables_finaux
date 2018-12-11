5e4b4ce0a3f908d2f05bad9ed8b788c4a5dc8557
loick legay <loick.legay@orange.fr>
2018-12-11 10:17:12 +0100

    Model update

--- Masterchief Project/RestaurantRoomConsole/Model/Model.cs
+++ Masterchief Project/RestaurantRoomConsole/Model/Model.cs
@@ -1,15 +1,78 @@
-﻿using System;
+﻿
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
+using RestaurantRoomConsole.Model;
 
-namespace Kitchen.Model
+namespace RestaurantRoomConsole.Model
 {
-    class Model
+    public class Modell
     {
-        public int clock;
-        Kitchen kitchen = Kitchen.GetInstance;
-        ExchangeDesk exchangeDesk = ExchangeDesk.GetInstance;
+
+        //default constructor
+
+        public int cntClient;
+        private int count;
+        public Modell()
+        {
+            this.count = 0;
+            this.cntClient = 0;
+            MaitreHotel mh = new MaitreHotel();
+            Restaurant restaurant = new Restaurant();
+
+            Console.WriteLine("Nombre de tables : " + Restaurant.listTables.Count);
+
+
+
+
+            // Loop pour générer les clients
+            Thread thgcl = new Thread(FirstLoop);
+            thgcl.Start();
+            Thread lpgenClient = new Thread(LoopGenClient);
+            lpgenClient.Start();
+        }
+
+        public void FirstLoop()
+        {
+
+            while (true)
+            {
+
+
+
+
+                foreach (Tables table in Restaurant.listTables)
+                {
+                    Console.Write("table: " + table.isOccuped + " | ");
+                }
+                Console.WriteLine();
+                Thread.Sleep(2000);
+
+
+
+            }
+        }
+
+        public void LoopGenClient()
+        {
+            while (true)
+            {
+                this.count += 1;
+                Random rnd = new Random();
+                int nbrClients = rnd.Next(2, 6);
+
+
+                GroupClient group = new GroupClient("group" + this.count, nbrClients);
+                Restaurant.listGroupClient.Add(group);
+
+                // Random entre 10 et 20 sec pour un nouveau groupe de client
+                Thread.Sleep(rnd.Next(3000, 6000));
+            }
+        }
     }
 }
+
