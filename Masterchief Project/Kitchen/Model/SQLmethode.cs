210e2daa4adabf58b377564bd49dd1865eba1d5f
XxxxPrInCeSs_Of_TrAcToRsxxxX <camille.aubret@viacesi.fr>
2018-12-13 15:57:21 +0100

    Update SQLmethode.cs

--- Masterchief Project/Kitchen/Model/SQLmethode.cs
+++ Masterchief Project/Kitchen/Model/SQLmethode.cs
@@ -6,74 +6,47 @@ using System.Threading.Tasks;
 
 namespace Kitchen
 {
-    class SQLmethode
+    static class SQLmethode
     {
-        private string rq_sql;
-        private string table;
-        private int category;
-        private int idRecipe;
+        static string rq_sql;
+        static string table;
+        static int category;
+        static int idRecipe;
 
         //accesseurs
-        public string Rq_sql { get => rq_sql; set => rq_sql = value; }
-        public string Table { get => table; set => table = value; }
-        public int Category { get => category; set => category = value; }
-        public int IdRecipe { get => idRecipe; set => idRecipe = value; }
+        static string Rq_sql { get => rq_sql; set => rq_sql = value; }
+        static string Table { get => table; set => table = value; }
+        static int Category { get => category; set => category = value; }
+        static int IdRecipe { get => idRecipe; set => idRecipe = value; }
 
         //Les méthodes
-        public string SelectAllFromTable(string ptable)
+        static string SelectAllFromTable(string ptable)
         {
             table = ptable;
 
             return Rq_sql = "SELECT * FROM " + table;
         }
 
-        public string SelectRecepiesByType(int pcategory)
+        static string SelectRecepiesByType(int pcategory)
         {
             category = pcategory;
 
             return Rq_sql = "SELECT IDRecette FROM Recette WHERE Categorie = " + category;
         }
 
-        public string SelectIngredientsAndQuantitiesByRecipes(int precipe)
+        static string SelectIngredientsAndQuantitiesByRecipes(int precipe)
         {
             idRecipe = precipe;
 
             return Rq_sql = "SELECT IDIngredient, QuantiteIngredient FROM Compose WHERE IDRecette =" + idRecipe;
         }
 
-        /*
-        public string SelectIngredientsAndQuantitiesByRecipes(int precipe)
+        static string UpdateIngredientStockByRecipe(int precipe)
         {
             idRecipe = precipe;
 
-            return Rq_sql = "SELECT IDIngredient, QuantiteIngredient FROM Compose WHERE IDRecette =" + idRecipe;
+            return Rq_sql = "EXEC SelectRecette @Recette = " + idRecipe;
         }
-        */
 
-
-        //public string delete(int pid)
-        //{
-        //    id = pid;
-
-        //    return Rq_sql = "DELETE FROM TB_A2_WS2 WHERE" + pid;
-        //}
-
-        //public string insert(int pid, string name, string firstname)
-        //{
-        //    id = pid;
-        //    nom = name;
-        //    prénom = firstname;
-
-        //    return Rq_sql = "INSERT INTO TB_A2_WS2 VALUES ('" + pid + "', '" + name + "','" + firstname + "')";
-        //}
-
-        //public string update(int pid, string name, string firstname)
-        //{
-        //    id = pid;
-        //    nom = name;
-        //    prénom = firstname;
-
-        //    return Rq_sql = "UPDATE TB_A2_WS2 SET nom = '" + name + "', prenom = '" + firstname + "' WHERE" + pid;
-        //}
     }
 }
