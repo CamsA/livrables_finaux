a721bd0c30a2012a1e0a9e91c20d2becc0be4dff
XxxxPrInCeSs_Of_TrAcToRsxxxX <camille.aubret@viacesi.fr>
2018-12-14 13:28:45 +0100

    DB

--- Masterchief Project/Kitchen/Model/SQLmethode.cs
+++ Masterchief Project/Kitchen/Model/SQLmethode.cs
@@ -6,32 +6,47 @@ using System.Threading.Tasks;
 
 namespace Kitchen
 {
-    static class SQLmethode
+    public static class SQLmethode
     {
-        static string rq_sql;
+        public static string rq_sql;
 
         //accesseur
-        static string Rq_sql { get => rq_sql; set => rq_sql = value; }
+        public static string Rq_sql { get => rq_sql; set => rq_sql = value; }
 
         //Les méthodes
-        static string SelectAllFromTable(string table)
+        public static string SelectAllFromTable(string table)
         {
             return Rq_sql = "SELECT * FROM " + table;
         }
 
-        static string SelectRecepiesByType(int category)
+        public static string SelectRecepiesByType(int category)
         {
             return Rq_sql = "SELECT IDRecette FROM Recette WHERE Categorie = " + category;
         }
 
-        static string SelectIngredientsAndQuantitiesByRecipes(int recipe)
+        public static string SelectIngredientsAndQuantitiesByRecipes(int recipe)
         {
             return Rq_sql = "SELECT IDIngredient, QuantiteIngredient FROM Compose WHERE IDRecette =" + recipe;
         }
 
-        static string UpdateIngredientStockByRecipe(int recipe)
+        public static string UpdateIngredientStockByRecipe(int recipe)
         {
             return Rq_sql = "EXEC SelectRecette @Recette = " + recipe;
+        }
+
+        public static string UpdateArrivalDayIngredientStockByRecipe(int recipe)
+        {
+            return Rq_sql = "UPDATE StockCuisine SET JourArrivé = JourArrivé + 1 WHERE IDStock = " + recipe;
+        }
+
+        public static string InsertIntoScenario(int path)
+        {
+            return Rq_sql = "INSERT INTO Scenario( CheminRacine, Date ) VALUES('" + path + "', GETDATE()); ";
+        }
+
+        public static string SelectTimeTasksByRecipes(int recipe)
+        {
+            return Rq_sql = "SELECT Tache.DureeTache FROM Tache LEFT JOIN Necessite On Necessite.IDTache = Tache.IDTache WHERE Necessite.IDRecette = " + recipe;
         }
     }
 }
