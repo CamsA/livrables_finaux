27ef6a2f24e0fbe66407ae4d3892a050388f5a21
XxxxPrInCeSs_Of_TrAcToRsxxxX <camille.aubret@viacesi.fr>
2018-12-16 18:42:10 +0100

    Add comment

--- Masterchief Project/Kitchen/Model/SQLprocess.cs
+++ Masterchief Project/Kitchen/Model/SQLprocess.cs
@@ -10,58 +10,65 @@ namespace Kitchen.Model
 {
     public static class SQLprocess
     {
+        //Define the new dataset to get data
         public static DataSet oDS;
         public static string rq_sql = "";
 
+        //Before call any of th method above, you need to call this one first
         public static void Start()
         {
             oDS = new DataSet();
             DBConnect.Start("MasterChiefDB");
         }
 
+
+        //-----------------------------------------------------------------------------
+        //  These are the methods you actually call when you need to excute a request
+        //-----------------------------------------------------------------------------
+
         public static DataSet DisplayAll(string dataTableName, string table)
         {
-            oDS.Clear();
-            oDS = DBConnect.GetRows(dataTableName, SQLmethode.SelectAllFromTable(table));
+            oDS.Clear(); //Clear the dataset from any prvious data 
+            oDS = DBConnect.GetRows(dataTableName, SQLmethode.SelectAllFromTable(table)); //As it is a SELECT request that we want to execute, we need GetRows
             return oDS;
         }
 
         public static DataSet GetRecipesByType(string dataTableName, int category)
         {
-            oDS.Clear();
-            oDS = DBConnect.GetRows(dataTableName, SQLmethode.SelectRecepiesByType(category));
+            oDS.Clear();//Clear the dataset from any prvious data
+            oDS = DBConnect.GetRows(dataTableName, SQLmethode.SelectRecepiesByType(category));//As it is a SELECT request that we want to execute, we need GetRows
             return oDS;
         }
 
         public static DataSet GetQuantiesByRecipes(string dataTableName, int category)
         {
-            oDS.Clear();
-            oDS = DBConnect.GetRows(dataTableName, SQLmethode.SelectRecepiesByType(category));
+            oDS.Clear();//Clear the dataset from any prvious data
+            oDS = DBConnect.GetRows(dataTableName, SQLmethode.SelectRecepiesByType(category));//As it is a SELECT request that we want to execute, we need GetRows
             return oDS;
         }
 
         public static void UpdateIngredientStock(int recipe)
         {
-            oDS.Clear();
-            DBConnect.ActionOnRows(SQLmethode.UpdateIngredientStockByRecipe(recipe));
+            oDS.Clear();//Clear the dataset from any prvious data
+            DBConnect.ActionOnRows(SQLmethode.UpdateIngredientStockByRecipe(recipe));//As we want to change something in the database, we need ActionOnRows
         }
 
         public static void UpdateIngredientDay(int recipe)
         {
-            oDS.Clear();
-            DBConnect.ActionOnRows(SQLmethode.UpdateArrivalDayIngredientStockByRecipe(recipe));
+            oDS.Clear();//Clear the dataset from any prvious data
+            DBConnect.ActionOnRows(SQLmethode.UpdateArrivalDayIngredientStockByRecipe(recipe));//As we want to change something in the database, we need ActionOnRows
         }
 
         public static void AddNewScenario(int path)
         {
-            oDS.Clear();
-            DBConnect.ActionOnRows(SQLmethode.InsertIntoScenario(path));
+            oDS.Clear();//Clear the dataset from any prvious data
+            DBConnect.ActionOnRows(SQLmethode.InsertIntoScenario(path));//As we want to change something in the database, we need ActionOnRows
         }
 
         public static DataSet GetTimeTasksByRecipes(string dataTableName, int recipe)
         {
-            oDS.Clear();
-            oDS = DBConnect.GetRows(dataTableName, SQLmethode.SelectTimeTasksByRecipes(recipe));
+            oDS.Clear();//Clear the dataset from any prvious data
+            oDS = DBConnect.GetRows(dataTableName, SQLmethode.SelectTimeTasksByRecipes(recipe));//As it is a SELECT request that we want to execute, we need GetRows
             return oDS;
         }
 
